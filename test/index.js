215e24979ee7daf54bb8eca4d27f22f32aa0277a
Santiago G. Marín <santiago@finaldevstudio.com>
2016-07-21 11:02:12 -0400

    Updated tests

--- test/index.js
+++ test/index.js
@@ -1,120 +1,47 @@
 'use strict';
 
-var expect = require('chai').expect;
-var mongoose = require('mongoose');
-var statics = require('../lib');
-var uuid = require('node-uuid');
-var path = require('path');
+const expect = require('chai').expect;
+const CONSTS = require('../lib');
 
-describe('Fi Statics', function () {
-
-  before(function (done) {
-
-    mongoose.connect('mongodb://localhost:27017/fi-statics-tests-' + uuid.v4());
-
-    mongoose.connection.once('open', function () {
-      var basedir = path.normalize(path.join(__dirname, 'schemas', 'static'));
-
-      mongoose.model('static.gender', require(basedir + '/gender')(mongoose.Schema));
-      mongoose.model('static.role', require(basedir + '/role')(mongoose.Schema));
-
-      mongoose.model('static.gender').create([{
-        slug: 'male'
-      }, {
-        slug: 'female'
-      }], function (err) {
-        if (err) {
-          done(err);
-        }
-      });
-
-      mongoose.model('static.role').create([{
-        name: 'User',
-        slug: 'user'
-      }, {
-        name: 'Administrator',
-        slug: 'admin'
-      }], function (err) {
-        if (err) {
-          done(err);
-        }
-      });
-
-      statics(mongoose, {
-        basedir: basedir,
-        debug: true
-      }, done);
-    });
-
-    mongoose.connection.on('error', done);
-
-  });
+describe('Fi Consts', function () {
 
   it('object should be a function', function () {
-    expect(statics).to.be.a('function');
+    expect(CONSTS).to.be.an('object');
   });
 
-  it('should retrieve all cached models', function () {
-    var data = statics.get();
-
-    expect(data).to.be.an('object');
-    expect(data.gender).to.be.an('object');
-    expect(data.gender.male).to.be.an('object');
-    expect(data.gender.female).to.be.an('object');
-    expect(data.role).to.be.an('object');
-    expect(data.role.admin).to.be.an('object');
-    expect(data.role.user).to.be.an('object');
+  it('should fail initialization if basedir is not present', function () {
+    expect(CONSTS.load.bind(null)).to.throw(Error);
   });
 
-  it('should retrieve a model\'s data', function () {
-    var data = statics.get('role');
-
-    expect(data).to.be.an('object');
-    expect(data.admin).to.be.an('object');
-    expect(data.user).to.be.an('object');
+  it('should fail initialization if basedir is not a string', function () {
+    expect(CONSTS.load.bind(null, {
+      basedir: 1234
+    })).to.throw(Error);
   });
 
-  it('should retrieve a single static ObjectID', function () {
-    var data = statics.get('role', 'admin');
+  it('should initialize properly if config is valid', function () {
+    expect(CONSTS.load.bind(null, {
+      basedir: __dirname + '/consts'
+    })).to.not.throw(Error);
 
-    expect(data).to.be.an('object');
-    expect(mongoose.Types.ObjectId.isValid(data.toString())).to.be.true;
-  });
+    expect(CONSTS).to.be.an('object');
 
-  it('should retrieve a single static data', function () {
-    var data = statics.get('role', 'admin', true);
-
-    expect(data).to.be.an('object');
-    expect(data.slug).to.be.a('string');
-    expect(data.name).to.be.a('string');
-    expect(data._id).to.be.an('object');
-  });
-
-  it('should reload statics contents', function (done) {
-
-    mongoose.model('static.role').create({
-      name: 'Moderator',
-      slug: 'mod'
-    }, function (err) {
-      if (err) {
-        done(err);
-      }
-
-      statics.reload(function () {
-        var data = statics.get('role', 'mod');
-
-        expect(data).to.be.an('object');
-
-        done();
-      });
-    });
-
-  });
-
-  after(function (done) {
-    mongoose.connection.db.dropDatabase(function (err, result) {
-      done(err);
-    });
+    expect(CONSTS).to.not.be.empty;
+    expect(CONSTS.ROLES).to.be.an('object');
+    expect(CONSTS.ROLES.ADMIN).to.be.an('object');
+    expect(CONSTS.ROLES.USER).to.be.an('object');
+    expect(CONSTS.ROLES.ADMIN.SLUG).to.be.a('string');
+    expect(CONSTS.ROLES.USER.SLUG).to.be.a('string');
+    expect(CONSTS.ROLES.ADMIN.VALUE).to.be.a('string');
+    expect(CONSTS.ROLES.USER.VALUE).to.be.a('string');
+    expect(CONSTS.OTHER).to.be.an('object');
+    expect(CONSTS.OTHER.VALUES).to.be.an('array');
+    expect(CONSTS.OTHER.EMBEDDED).to.be.an('object');
+    expect(CONSTS.OTHER.EMBEDDED.VALUES).to.be.an('object');
+    expect(CONSTS.OTHER.EMBEDDED.VALUES.VALUE_1).to.be.an('object');
+    expect(CONSTS.OTHER.EMBEDDED.VALUES.VALUE_2).to.be.an('object');
+    expect(CONSTS.OTHER.EMBEDDED.VALUES.VALUE_1.ARRAY).to.be.an('array');
+    expect(CONSTS.OTHER.EMBEDDED.VALUES.VALUE_2.ARRAY).to.be.an('array');
   });
 
 });
